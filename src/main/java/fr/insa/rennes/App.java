20642b7fef8c2d106eae34e09fd8ee9dd74830c0
Julien Marchais <Julien.Marchais@insa-rennes.fr>
2015-10-19 16:26:42 +0200

    1st commit

---
+++ src/main/java/fr/insa/rennes/App.java
@@ -0,0 +1,17 @@
+package fr.insa.rennes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     * Main Commentaire
+     * @param args arguments
+     */
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
